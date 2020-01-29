f842372115bb44e2ab8f6dec2b0251b2f0ed5cab
kryst1anb <48355396+kryst1anb@users.noreply.github.com>
2020-01-29 22:22:10 +0100

    12321231321321321321

---
+++ kalkulator/app/src/main/java/com/example/kalkulator/ui/ProductSearchViewModel.kt
@@ -0,0 +1,15 @@
+package com.example.kalkulator.ui
+
+import androidx.lifecycle.ViewModel
+import com.example.kalkulatorkalorii.json.ResultProductList
+
+class ProductSearchViewModel : ViewModel() {
+
+    private var searchedList = ResultProductList().listItems
+
+   /* fun getDataFromAPI(typedSearch: String): ResultProductList {
+        val dataFromApi = apiConnect().apiConnection(typedSearch)
+
+       return dataFromApi
+    }*/
+}
---
+++ kalkulator/app/src/main/java/com/example/kalkulator/ui/ViewModelList.kt
@@ -0,0 +1,5 @@
+package com.example.kalkulator.ui
+
+class ViewModelList {
+
+}
