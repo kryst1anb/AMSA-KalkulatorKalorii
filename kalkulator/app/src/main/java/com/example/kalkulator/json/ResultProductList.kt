e3e61092ceb088bacbd492873d701d0344b34bbd
kryst1anb <48355396+kryst1anb@users.noreply.github.com>
2020-01-30 10:23:34 +0100

    123

--- kalkulator/app/src/main/java/com/example/kalkulator/json/ResultProductList.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kalkulatorkalorii.json
-
-import com.google.gson.annotations.SerializedName
-
-class ResultProductList {
-    @field:SerializedName("Results") var listItems: List<ResultProduct?>? = null
-}
